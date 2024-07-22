8deb7f6b4bfb3d0c90078d1bb706ce6153e2237c
unknown <blandinedjoumsie@gmail.com>
2024-07-22 20:19:45 +0200

    added variables, arrays and job to my script, split them and added some semantics

--- Script.js
+++ Script.js
@@ -1,7 +1,18 @@
 let greetingText = "Hi I'm Julie"
-let YourAge = 32
+
 alert(greetingText)
 alert(greetingText) 
 greetingText = "Hi I, am really Julie"
 alert(greetingText) 
+
+let YourAge = 32
 alert(YourAge)
+
+let hobbies = ["sports", "cooking", "reading"]
+alert(hobbies[0])
+
+let job = {
+    title: "mecanic",
+     place: "bonamoussadi",
+      salary: "60000"
+}
