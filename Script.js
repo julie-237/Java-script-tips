ab60412d066cb5058bd6fed9938f13fc11ea4883
unknown <blandinedjoumsie@gmail.com>
2024-07-24 10:24:29 +0200

    worked on objects, operations and calculations

--- Script.js
+++ Script.js
@@ -1,18 +1,24 @@
-let greetingText = "Hi I'm Julie"
+let greetingText = "Hi I'm Julie";
 
-alert(greetingText)
-alert(greetingText) 
-greetingText = "Hi I, am really Julie"
-alert(greetingText) 
+alert(greetingText);
+alert(greetingText);
+greetingText = "Hi I, am really Julie";
+alert(greetingText);
 
-let YourAge = 32
-alert(YourAge)
+let YourAge = 32;
+alert(YourAge);
 
-let hobbies = ["sports", "cooking", "reading"]
-alert(hobbies[0])
+let hobbies = ["sports", "cooking", "reading"];
+alert(hobbies[0]);
 
 let job = {
-    title: "mecanic",
-     place: "bonamoussadi",
-      salary: "60000"
-}
+  title: "mecanic",
+  place: "bonamoussadi",
+  salary: "60000",
+};
+alert(job.title)
+
+let adultyears = YourAge - 18
+alert(adultyears)
+YourAge = 45
+
