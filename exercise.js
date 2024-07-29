84f36f29dd8ce6b8f08e8f0fb8425b2584afbac0
unknown <blandinedjoumsie@gmail.com>
2024-07-29 20:56:56 +0200

    created the first three variables and output them

---
+++ exercise.js
@@ -0,0 +1,12 @@
+let CourseName = "100days of code";
+alert(CourseName);
+
+let CoursePrice = "34.99 US";
+alert(CoursePrice);
+
+let courseGoals = [
+  "Creativity",
+  "Personal skills development",
+  "Develop problem-solving",
+];
+alert(courseGoals[1]);
