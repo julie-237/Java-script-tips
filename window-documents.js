aa4d2030cab0d7b5c6b3629ee4f7de158f6a67b7
unknown <blandinedjoumsie@gmail.com>
2024-09-28 11:54:28 +0200

    Inserted a new element (anchor element) into the DOM and configured it in detail

--- window-documents.js
+++ window-documents.js
@@ -1,3 +1,14 @@
 //document.body.children[1].children[0].href = "https://google.com";
 //console.dir(document)
 document.getElementById("external-link").href = "https://google.com";
+
+
+// Inserting new HTML element via Javascript
+// 1. create a new element
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://google.com"
+newAnchorElement.textContent = "This leads to Google!"
+// 2. Get access to the parent element that should hold the new element
+let firstParagraph = document.querySelector("p")
+//3. Insert the new element into the parent element content
+firstParagraph.append(newAnchorElement)
