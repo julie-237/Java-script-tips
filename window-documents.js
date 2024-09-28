09495fd18a06f5673cd469de6329bfabcd86f7a6
unknown <blandinedjoumsie@gmail.com>
2024-09-28 12:08:51 +0200

    removed the h1 element from the DOM

--- window-documents.js
+++ window-documents.js
@@ -12,3 +12,12 @@ newAnchorElement.textContent = "This leads to Google!"
 let firstParagraph = document.querySelector("p")
 //3. Insert the new element into the parent element content
 firstParagraph.append(newAnchorElement)
+
+
+// Removing an HTML element via Javascript
+// 1. Select the element that should be removed
+let firstH1Element = document.querySelector("h1");
+// 2. Remove it
+firstH1Element.remove();
+//firstH1Element.parentElement.removeChild(firstH1Element);// for older browsers
+
